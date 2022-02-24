99209c0bbd6b6a8913611716478dce838b2b51f9
slumsd01 <90315740+slumsd01@users.noreply.github.com>
2022-02-24 02:39:38 -0500

    21.3 start

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express')
-const {typeDefs, resolvers} = require('./schemas')
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -10,11 +13,12 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-  })
-  await server.start()
-  server.applyMiddleware({app})
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
-}
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+};
 
 startServer()
 
